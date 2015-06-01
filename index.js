22b1a350b10632f7005f9db6cf65baa8c661e3ae
Xavi Lozano Segarra <xavils@Xavis-MacBook-Air.local>
2015-06-01 11:05:31 +0800

    change mongo url

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var plugins = [
   { 
     register: require('hapi-mongodb'),
     options: {
-      'url' : 'mongodb://127.0.0.1:27017/hashtopics',
+      'url' : 'mongodb://<dbuser>:<dbpassword>@ds043012.mongolab.com:43012/heroku_app37366178',
       'settings' : {
         'db': {
           'native_parser': true
